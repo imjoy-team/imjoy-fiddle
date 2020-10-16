7ba2cba7731060025e14c1e3f4858861fa8e5180
oeway <oeway@users.noreply.github.com>
2020-10-16 00:13:53 +0000

    Deploy to GitHub Pages 52168fcb9b8ea58831a5bfccfd04dda65d65bd76

---
+++ precache-manifest.8377222ead5eeddbbb8792647ec92f35.js
@@ -0,0 +1,198 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8762a8b2c145876dfca32839f0c9214",
+    "url": "/CNAME"
+  },
+  {
+    "revision": "d369dca74cf8cfca41fa",
+    "url": "/css/app.ec94c57d.css"
+  },
+  {
+    "revision": "ef215772da122880fd8b",
+    "url": "/css/chunk-vendors.d031d295.css"
+  },
+  {
+    "revision": "6a2ddad1092a0a1c326b6d0e738e682b",
+    "url": "/fonts/materialdesignicons-webfont.6a2ddad1.ttf"
+  },
+  {
+    "revision": "8a67040660444e8d8ee82a406d580d8f",
+    "url": "/fonts/materialdesignicons-webfont.8a670406.eot"
+  },
+  {
+    "revision": "c32505e8c654310ece1da4e1c10eeb57",
+    "url": "/fonts/materialdesignicons-webfont.c32505e8.woff"
+  },
+  {
+    "revision": "dc85ceeb0daba687e36d8dde4ed4d352",
+    "url": "/fonts/materialdesignicons-webfont.dc85ceeb.woff2"
+  },
+  {
+    "revision": "355ff928d04b63f140f4badbd98713cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c263760b078a63e4804d",
+    "url": "/js/about.1917e56e.js"
+  },
+  {
+    "revision": "d369dca74cf8cfca41fa",
+    "url": "/js/app.9d3355e7.js"
+  },
+  {
+    "revision": "ef215772da122880fd8b",
+    "url": "/js/chunk-vendors.f1b7b97a.js"
+  },
+  {
+    "revision": "c88deb950f323547f0a75bd2c8cde093",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "7a4ca1ad9bc43e28ad66b04df8330a3a",
+    "url": "/static/badge/open-with-kaibu.svg"
+  },
+  {
+    "revision": "1167d30fc84e918bbd671966b4c16dc0",
+    "url": "/static/badge/view-in-kaibu.svg"
+  },
+  {
+    "revision": "cc19fd84e6a224e2e569cc62f1a6ff97",
+    "url": "/static/icons/android-icon-144x144.png"
+  },
+  {
+    "revision": "0580fd867305e330cffb8d6facd2465a",
+    "url": "/static/icons/android-icon-192x192.png"
+  },
+  {
+    "revision": "93a616451a7267844b6ed06fde5c6ecf",
+    "url": "/static/icons/android-icon-36x36.png"
+  },
+  {
+    "revision": "1559010b7d933d24cb60bb9993e412b1",
+    "url": "/static/icons/android-icon-48x48.png"
+  },
+  {
+    "revision": "d4e4d1ebc40e5509feaf1715cb135f96",
+    "url": "/static/icons/android-icon-72x72.png"
+  },
+  {
+    "revision": "51eee4c6a2cbff5e6d149f82d12699a2",
+    "url": "/static/icons/android-icon-96x96.png"
+  },
+  {
+    "revision": "552a2730b5ce83d66f7804ef2e3a1715",
+    "url": "/static/icons/apple-icon-114x114.png"
+  },
+  {
+    "revision": "46d69724e0b5e48b24e3c7ab12606d58",
+    "url": "/static/icons/apple-icon-120x120.png"
+  },
+  {
+    "revision": "cc19fd84e6a224e2e569cc62f1a6ff97",
+    "url": "/static/icons/apple-icon-144x144.png"
+  },
+  {
+    "revision": "edba2cda2b6fa6c55e9c7c336d2de3b4",
+    "url": "/static/icons/apple-icon-152x152.png"
+  },
+  {
+    "revision": "4efc4b6b32a094bfb5aaccbbff3ea510",
+    "url": "/static/icons/apple-icon-180x180.png"
+  },
+  {
+    "revision": "4b6ff72f7ae842e59cc2f40cba35ff52",
+    "url": "/static/icons/apple-icon-57x57.png"
+  },
+  {
+    "revision": "66e1572df15e1f7f64d1dad457af1743",
+    "url": "/static/icons/apple-icon-60x60.png"
+  },
+  {
+    "revision": "d4e4d1ebc40e5509feaf1715cb135f96",
+    "url": "/static/icons/apple-icon-72x72.png"
+  },
+  {
+    "revision": "f25abb80b967186043c196bc022648f6",
+    "url": "/static/icons/apple-icon-76x76.png"
+  },
+  {
+    "revision": "77d9f96585bb42dcb73f56bc7cc594a4",
+    "url": "/static/icons/apple-icon-precomposed.png"
+  },
+  {
+    "revision": "77d9f96585bb42dcb73f56bc7cc594a4",
+    "url": "/static/icons/apple-icon.png"
+  },
+  {
+    "revision": "653d077300a12f09a69caeea7a8947f8",
+    "url": "/static/icons/browserconfig.xml"
+  },
+  {
+    "revision": "90a7b93bdd8acaa5cb221740e98415a0",
+    "url": "/static/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "d765755b7bd60885245ab699a2dd77b3",
+    "url": "/static/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "51eee4c6a2cbff5e6d149f82d12699a2",
+    "url": "/static/icons/favicon-96x96.png"
+  },
+  {
+    "revision": "cc19fd84e6a224e2e569cc62f1a6ff97",
+    "url": "/static/icons/ms-icon-144x144.png"
+  },
+  {
+    "revision": "1c2724eb80a0c1db3ef11921705e2afd",
+    "url": "/static/icons/ms-icon-150x150.png"
+  },
+  {
+    "revision": "2e403999247f5cfd34f041323456f6eb",
+    "url": "/static/icons/ms-icon-310x310.png"
+  },
+  {
+    "revision": "de8c5ee9e2d58425c8bdb275aa03aa01",
+    "url": "/static/icons/ms-icon-70x70.png"
+  },
+  {
+    "revision": "ffa4942f49d8b770147f2063891d4dda",
+    "url": "/static/img/kaibu-banner-trans.png"
+  },
+  {
+    "revision": "3576f4b63dcf6a6d194da94660cc3e8c",
+    "url": "/static/img/kaibu-banner.png"
+  },
+  {
+    "revision": "07171f634e995aaeb71a9bbeaf15f3df",
+    "url": "/static/img/kaibu-banner.svg"
+  },
+  {
+    "revision": "ea3c38f21da766d38fd3fb0a1f75e005",
+    "url": "/static/img/kaibu-icon-trans.png"
+  },
+  {
+    "revision": "42b300fac246f9a92cce1d05728e2b4d",
+    "url": "/static/img/kaibu-icon.png"
+  },
+  {
+    "revision": "c63a2bf3b89bb6d9d9b19fd86d2c3976",
+    "url": "/static/img/kaibu-icon.svg"
+  },
+  {
+    "revision": "685701402a6b7689eb794924f26b3caa",
+    "url": "/static/img/kaibu-logo-trans.png"
+  },
+  {
+    "revision": "abd3ca42b535a0aca482f14051699438",
+    "url": "/static/img/kaibu-logo.gif"
+  },
+  {
+    "revision": "9c70934a3e14acf831670f9222890b38",
+    "url": "/static/img/kaibu-screenshot-1.png"
+  }
+]);
